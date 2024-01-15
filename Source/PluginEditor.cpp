ee31d82855c38a6a228cbc9adb5213f7db0edf89
Ashu Desai <ashuddesai@gmail.com>
2024-01-14 23:35:15 -0800

    ableton debugging working

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -8,14 +8,37 @@
 
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
+#include <juce_core/juce_core.h>
 
 //==============================================================================
 NewProjectAudioProcessorEditor::NewProjectAudioProcessorEditor (NewProjectAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    
+    setSize (800, 600); // Set a larger size for the editor to accommodate the browser
+
+    addAndMakeVisible(browser); // Make the browser visible in this component
+//    browser.goToURL("https://dev.808gpt.xyz"); // Load a webpage
+    
+    juce::File htmlFile = juce::File::getSpecialLocation(juce::File::currentExecutableFile).getParentDirectory().getParentDirectory().getChildFile("Resources/index.html");
+    
+    DBG("Your log message here");
+    DBG(htmlFile.getFullPathName());
+    
+    juce::Logger::writeToLog("dfasdfasdfasdfsad");
+    
+    std::cout << "ASHU";
+    
+    if (htmlFile.exists())
+    {
+        juce::URL localHtmlUrl = juce::URL(htmlFile);
+        browser.goToURL(localHtmlUrl.toString(false));
+    }
+    else
+    {
+        // Handle the error in case the file does not exist
+        browser.goToURL("https://studiocollective.xyz");
+    }
 }
 
 NewProjectAudioProcessorEditor::~NewProjectAudioProcessorEditor()
@@ -26,15 +49,17 @@ NewProjectAudioProcessorEditor::~NewProjectAudioProcessorEditor()
 void NewProjectAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+//    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+//
+//    g.setColour (juce::Colours::white);
+//    g.setFont (15.0f);
+//    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void NewProjectAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    
+    browser.setBounds(getLocalBounds());
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -10,6 +10,7 @@
 
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
+#include <juce_gui_extra/juce_gui_extra.h>
 
 //==============================================================================
 /**
@@ -28,6 +29,7 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     NewProjectAudioProcessor& audioProcessor;
+    juce::WebBrowserComponent browser;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (NewProjectAudioProcessorEditor)
 };
